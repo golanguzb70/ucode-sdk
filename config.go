5c80ba24bb7e46c258bcdd96c1a2b895ac4dfd5b
akhmedov01 <akhmedovdilmurodjon01@gmail.com>
2024-01-25 15:38:56 +0500

    no comment

--- config.go
+++ config.go
@@ -7,19 +7,3 @@ type Config struct {
 	BotToken   string
 	AccountIds []string
 }
-
-func (cfg *Config) SetAppId(appId string) {
-	cfg.AppId = appId
-}
-
-func (cfg *Config) SetBaseUrl(url string) {
-	cfg.BaseURL = url
-}
-
-func (cfg *Config) SetTableSlug(slug string) {
-	cfg.TableSlug = slug
-}
-
-func (cfg *Config) SetBotToken(token string) {
-	cfg.BotToken = token
-}
